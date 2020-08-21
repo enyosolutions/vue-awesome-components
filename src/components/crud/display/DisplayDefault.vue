d20874dbc4cef704cc9339acb7ea3961b0f16758
Julien <julienmartin@codeassist.io>
2020-08-21 11:26:38 +0200

    Fix processus color

--- src/components/crud/display/DisplayDefault.vue
+++ src/components/crud/display/DisplayDefault.vue
@@ -24,28 +24,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-  .ajax-table-col-failed.ajax-table-col-value-failed {
-    color: #ffffff !important;
-    background-color: #87cb16!important;
-  }
-
-  .ajax-table-col-scheduled.ajax-table-col-value-scheduled {
-    color: #ffffff !important;
-    background-color: #ddd !important;
-  }
-
-  .ajax-table-col-running.ajax-table-col-value-running {
-    color: #ffffff !important;
-    background-color: #17a2b8!important;
-  }
-
-  .ajax-table-col-timeout.ajax-table-col-value-timeout {
-    color: #ffffff !important;
-    background-color: #caca08!important;
-  }
-  .ajax-table-col-completed.ajax-table-col-value-completed {
-    color: #ffffff !important;
-    background-color: #87cb16!important;
-  }
-</style>
+<style></style>
