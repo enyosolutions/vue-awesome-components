5ed8e561a2a14abd6bfbf25dce4c9a0438eabaa8
fb@enyosolutions.com <fb@enyosolutions.com>
2020-10-07 16:30:36 +0100

    doc: update of the documentation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,10 +51,10 @@ module.exports = {
         sidebarDepth: 2, // optional, defaults to 1
         children: [
           {
-            title: "AwesomeCrud",
+            title: "Awesome Crud",
             path: "/guide/awesomecrud/",
             sidebarDepth: 2,
-            collapsable: false,
+            collapsable: true,
             children: [
               { title: "Introduction", path: "/guide/awesomecrud/" },
               {
@@ -72,9 +72,9 @@ module.exports = {
             ]
           },
           {
-            title: "EnyoAjaxTable",
+            title: "Awesome Table",
             path: "/guide/ajaxtable/",
-            sidebarDepth: 2,
+            sidebarDepth: 1,
             collapsable: true,
             children: [
               { title: "Presentation", path: "/guide/ajaxtable/" },
@@ -84,13 +84,14 @@ module.exports = {
               }
             ]
           },
-          { title: "AwesomeList component", path: "/guide/awesomelist/" },
+          { title: "Awesome Kanban", path: "/guide/awesomekanban/" },
+          { title: "Awesome List", path: "/guide/awesomelist/" },
+          { title: "Awesome Layout", path: "/guide/awesomelayout/" },
           { title: "TableAndChartsCard", path: "/guide/tableandchartscard" },
           { title: "EnyoStats", path: "/guide/enyostats.md" },
           { title: "EnyoChartCard", path: "/guide/enyochartcard.md" },
           { title: "EnyoSelect", path: "/guide/enyoselect.md" },
-          { title: "AwesomeKanban", path: "/guide/awesomekanban/" },
-          { title: "AwesomeLayout", path: "/guide/awesomelayout/" }
+
         ]
       },
       {
