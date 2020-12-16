3be56e79f5cbe95e042febd089150165b6434d6a
NitramJulien <julienmartin@codeassist.io>
2020-12-16 11:20:18 +0100

    feat: Allow to open DisplayImage in new tab

--- src/components/crud/display/DisplayImage.vue
+++ src/components/crud/display/DisplayImage.vue
@@ -1,8 +1,13 @@
 <template>
   <div v-bind="$props" :class="$props.classes" :style="$props.styles">
-    <span class="pointer" v-if="$props.src">
+    <div class="pointer" v-if="$props.src">
       <img :src="$props.src" alt="image" class="aw-display-image-img" />
-    </span>
+      <div class="image-actions" v-if="value">
+        <button @click.prevent="openImageInTab($props.src)" type="button" class="btn btn-alt-style btn-sm p-2">
+          <i class="fa fa-external-link"></i>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,12 +16,38 @@ import awesomeDisplayMixin from '../../../mixins/displayMixin';
 
 export default {
   name: 'DisplayImage',
-  mixins: [awesomeDisplayMixin]
+  mixins: [awesomeDisplayMixin],
+  methods: {
+    openImageInTab(src) {
+      window.open(src, '_blank');
+    },
+
+  }
 };
 </script>
 
-<style>
+<style lang="scss">
 .aw-display-image-img {
   width: 100%;
+}
+.pointer {
+  position: relative;
+  width: 100%;
+}
+.image-actions {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-end;
+  align-items: flex-start;
+  padding: 5px;
+  button {
+    color: #fff;
+    background: var(--primary, #fff);
+    border-color: var(--primary, #fff) !important;
+  }
 }
 </style>
