191d051a887e21faa13a3194a43defc9a7b22543
Enyosolutions <fb@enyosolutions.com>
2021-01-13 12:30:58 +0000

    Update AwesomeTable.vue

--- src/components/table/AwesomeTable.vue
+++ src/components/table/AwesomeTable.vue
@@ -174,7 +174,7 @@
         <slot name="table-subtitle" />
       </p>
       <button
-        v-if="collapse"
+        v-if="collapsible"
         class="btn btn-i"
         data-toggle="collapse"
         :data-target="'#awTable-' + this._uid || this.uuid"
@@ -185,7 +185,7 @@
     </div>
     <div
       class="card-body aw-table-card-body collapse show"
-      :class="collapse ? 'collapse show' : ''"
+      :class="collapsible ? 'collapse show' : ''"
       :id="'awTable-' + this._uid || this.uuid"
     >
       <awesome-filter
@@ -548,7 +548,7 @@ export default {
       type: String,
       values: ['local', 'remote']
     },
-    collapse: {
+    collapsible: {
       default: true,
       type: Boolean,
       description: 'Wether the table can be collapsed using the minus button at the topRight corner.'
