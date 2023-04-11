44683e22d4118d3415e4afed8a466c60e7ac67ea
fb@enyosolutions.com <fb@enyosolutions.com>
2023-04-11 18:06:13 +0100

    v1.59.12

---
+++ src/components/crud/fields/parts/UploadButtonToggle.vue
@@ -0,0 +1,29 @@
+<template>
+  <small @click="onClick" class="text-right">
+    <i
+      class="fa text-primary"
+      :class="{
+        'fa-upload': !value,
+        'fa-link': value
+      }"
+    ></i>
+    {{ value ? $t('AwesomeForm.labels.provideFileInstead') : $t('AwesomeForm.labels.provideUrlInstead') }}
+  </small>
+</template>
+<script>
+export default {
+  name: 'UploadButtonToggle',
+  props: {
+    value: Boolean
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onClick() {
+      this.$emit('input', !this.value);
+      this.$emit('change', !this.value);
+    }
+  }
+};
+</script>
